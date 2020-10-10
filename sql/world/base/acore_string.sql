1cb35874c9e1b87c6f5df21dabe440d11159789a
Pagani Walter <paganiwalter@hotmail.com>
2020-10-10 12:53:44 -0300

    The message is added to the DB so that it can be translated.

---
+++ sql/world/base/acore_string.sql
@@ -0,0 +1,5 @@
+
+DELETE FROM `acore_string` WHERE `entry`=40000;
+
+INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
+(40000, "This server is running the |cff4CFF00Npc Promotion |rmodule.", NULL, NULL, NULL, NULL, NULL, "Ese servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", "Ese servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", NULL);
