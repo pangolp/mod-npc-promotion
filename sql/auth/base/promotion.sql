ada8e735a66311c81727365ef2821ec25e917661
Pagani Walter <paganiwalter@hotmail.com>
2020-10-10 12:14:50 -0300

    Adding id attribute to promotion

--- sql/auth/base/promotion.sql
+++ sql/auth/base/promotion.sql
@@ -1,8 +1,10 @@
 
 CREATE TABLE IF NOT EXISTS `promotion` (
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`accountId` int(11) unsigned NOT NULL,
 	`accountName` varchar(32) NOT NULL DEFAULT '',
 	`characterName` varchar(12) NOT NULL DEFAULT '',
 	`ip` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-	`date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+	`date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
