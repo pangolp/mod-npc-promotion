1c256f951ca1d99e070d062e1d59f3b89b74b7b0
Pagani Walter <paganiwalter@gmail.com>
2020-10-09 23:37:31 -0300

    Moving the database to the auth table

--- sql/auth/base/promotion.sql
+++ sql/auth/base/promotion.sql
@@ -2,5 +2,7 @@
 CREATE TABLE IF NOT EXISTS `promotion` (
 	`accountId` int(11) unsigned NOT NULL,
 	`accountName` varchar(32) NOT NULL DEFAULT '',
-	`characterName` varchar(12) NOT NULL DEFAULT ''
+	`characterName` varchar(12) NOT NULL DEFAULT '',
+	`ip` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+	`date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
