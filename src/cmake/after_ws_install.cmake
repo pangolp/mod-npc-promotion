a9f4bc86f305a6ad4276267607a9e99506ca0499
Nicolas Aravena <aravenam.nicolas@gmail.com>
2020-12-06 00:00:02 -0300

    Fix after cmake process

--- src/cmake/after_ws_install.cmake
+++ src/cmake/after_ws_install.cmake
@@ -2,14 +2,14 @@ if( WIN32 )
   if ( MSVC )
     add_custom_command(TARGET worldserver
       POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_PVPTITLES_DIR}/conf/mod_npc_promotion.conf.dist" ${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/
+      COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_NPC_PROMOTION_DIR}/conf/mod_npc_promotion.conf.dist" ${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/
     )
   elseif ( MINGW )
     add_custom_command(TARGET worldserver
       POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_PVPTITLES_DIR}/conf/mod_npc_promotion.conf.dist" ${CMAKE_BINARY_DIR}/bin/
+      COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_NPC_PROMOTION_DIR}/conf/mod_npc_promotion.conf.dist" ${CMAKE_BINARY_DIR}/bin/
     )
   endif()
 endif()
 
-install(FILES "${CMAKE_MOD_PVPTITLES_DIR}/conf/mod_npc_promotion.conf.dist" DESTINATION ${CONF_DIR})
+install(FILES "${CMAKE_MOD_NPC_PROMOTION_DIR}/conf/mod_npc_promotion.conf.dist" DESTINATION ${CONF_DIR})
