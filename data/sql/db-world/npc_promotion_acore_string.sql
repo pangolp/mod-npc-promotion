1f99dbb886a1f2d8139ed4a737aa3be5c210e255
Paul L.C <cn.paullee@gmail.com>
2023-04-23 19:56:33 +0800

    add locale

--- data/sql/db-world/npc_promotion_acore_string.sql
+++ data/sql/db-world/npc_promotion_acore_string.sql
@@ -2,4 +2,4 @@
 DELETE FROM `acore_string` WHERE `entry`=40000;
 
 INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
-(40000, "This server is running the |cff4CFF00Npc Promotion |rmodule.", NULL, NULL, NULL, NULL, NULL, "Este servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", "Este servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", NULL);
+(40000, "This server is running the |cff4CFF00Npc Promotion |rmodule.", NULL, NULL, NULL, "已启用 |cff4CFF00直升服务NPC |r模块.", NULL, "Este servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", "Este servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", NULL);
