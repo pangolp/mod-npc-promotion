80eaadd46ab6ebeb0d553802a1b3bac734c41e16
Pagani Walter <paganiwalter@gmail.com>
2023-07-20 04:24:07 -0300

    feat add zhCN locale string

--- data/sql/db-world/base/npc_promotion_acore_string.sql
+++ data/sql/db-world/base/npc_promotion_acore_string.sql
@@ -2,4 +2,4 @@ SET @ENTRY:=40000;
 
 DELETE FROM `acore_string` WHERE `entry`=@ENTRY;
 INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
-(@ENTRY, "This server is running the |cff4CFF00Npc Promotion |rmodule.", NULL, NULL, NULL, NULL, NULL, "Este servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", "Este servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", NULL);
+(@ENTRY, "This server is running the |cff4CFF00Npc Promotion |rmodule.", NULL, NULL, NULL, "已启用 |cff4CFF00直升服务NPC |r模块.", NULL, "Este servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", "Este servidor está utilizando |cff4CFF00 mod-npc-promotion |r.", NULL);
--- data/sql/db-world/base/npc_promotion_creature_template.sql
+++ data/sql/db-world/base/npc_promotion_creature_template.sql
@@ -7,3 +7,7 @@ SET @MODELID1:=22234;
 DELETE FROM `creature_template` WHERE `entry`=@ENTRY;
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
 (@ENTRY, 0, 0, 0, 0, 0, @MODELID1, 0, 0, 0, @NAME, @SUBNAME, NULL, @GOSSIP, 83, 83, 0, 35, 1, 1, 1, 0, 0, 20, 2, 0, 0, 0, 0, 0, 1, 1, 1, 768, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 'npc_promocion', 12340);
+
+DELETE FROM `creature_template_locale` WHERE `entry`=@ENTRY;
+INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES
+(@ENTRY, "zhCN", "新人伴手礼", "直升服务", 18019);
